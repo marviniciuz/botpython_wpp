f7eaf258f7751f8f47c1f7f5520042d87932ed36
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2023-07-10 22:33:17 -0300

    remove contacts

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ import keyboard
 import time
 from datetime import datetime
 
-contacts = ['+5563992688030','+5563984070060']
+contacts = [' ',' ']
 
 while len(contacts) >= 1:
     pywhatkit.sendwhatmsg(contacts[0],'ola mundo',datetime.now().hour,datetime.now().minute + 2)
